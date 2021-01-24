b502dba523037bbe5270b811a11a4a9ea68c6901
sahil bansal <bansalsahil740@gmail.com>
2021-01-24 21:03:25 +0530

    first commit

---
+++ sum.java
@@ -0,0 +1,17 @@
+package com.company;
+import java.util.Scanner;
+public class sum {
+
+    public static void main(String[] args) {
+      Scanner sc = new Scanner(System.in);
+        System.out.println("Enter number 1-");
+        int a=sc.nextInt();
+        System.out.println("Enter number 2-");
+        int b=sc.nextInt();
+        int sum = a + b;
+        System.out.println("sum of these two number is "+sum);
+    }
+}
+
+
+
