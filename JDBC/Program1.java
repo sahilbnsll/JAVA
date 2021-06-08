36a53f8be7cd14e82bbc28888be3c6891fbc1768
Sahil Bansal <bansalsahil740@gmail.com>
2021-06-08 23:53:21 +0530

    Connecting java Program to DataBase that insert values in the created table

--- JDBC/Program1.java
+++ JDBC/Program1.java
@@ -1,25 +1,48 @@
-import java.sql.Connection;
-import java.sql.DriverManager;
-import java.sql.Statement;
- 
-public class JdbcConnection {
- 
+import java.sql.*;
+import java.util.Scanner;
+
+public class Task_1 {
     public static void main(String[] args) {
         try {
             Class.forName("com.mysql.jdbc.Driver");
             Connection con= DriverManager.getConnection("jdbc:mysql://localhost:3306/lab_exp","root","sahil");
- 
-            String g = "create table Employee(id int(10) primary key , name varchar(30) not null, salary int(10), phone int(12), allowance int(10))";
- 
-            Statement stmt = con.createStatement();
-            stmt.execute(g);
- 
-            System.out.println("table created in Database....");
+
+            String g  = "insert into Employee(id ,name, salary, phone, allowance) values( ?, ?, ?, ?, ?)";
+
+            PreparedStatement ptsmt = con.prepareStatement(g);
+
+            Scanner sc = new Scanner(System.in);
+
+            for(int i=0;i<5;i++){
+                System.out.println("Enter ID :");
+                int id = sc.nextInt();
+
+                System.out.println("Enter Name:");
+                String Name = sc.next();
+
+                System.out.println("Enter Salary:");
+                int salary = sc.nextInt();
+
+                System.out.println("Enter Phone:");
+                long phone = sc.nextLong();
+
+                System.out.println("Enter Allowance:");
+                int allowance = sc.nextInt();
+
+                //set the values
+                ptsmt.setInt(1, id);
+                ptsmt.setString(2, Name);
+                ptsmt.setInt(3, salary);
+                ptsmt.setInt(4, (int) phone);
+                ptsmt.setInt(5, allowance);
+
+                ptsmt.executeUpdate();
+                System.out.println("");
+            }
+            System.out.println("Inserted Successfully...");
+
             con.close();
         }
-        catch(Exception e){
-            e.printStackTrace();
-        }
+        catch (Exception e) { e.printStackTrace(); }
     }
 }
-
